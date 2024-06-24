1155c9004adaa5519e1ce2c98d763ffa0f218241
maxlair1 <max@lairfamily.com>
2024-06-24 10:00:49 -0400

    Primer Button

--- habbi-app/src/app/navbar.tsx
+++ habbi-app/src/app/navbar.tsx
@@ -1,11 +1,87 @@
-import { Header, Octicon, Avatar } from "@primer/react";
+"use client";
 
-const navbar = () => {
+import { Header, Octicon, Avatar, ActionMenu, ActionList } from "@primer/react";
+import {
+  CopyIcon,
+  ArchiveIcon,
+  WorkflowIcon,
+  GearIcon,
+  BookIcon,
+  CommentIcon,
+  RocketIcon,
+} from "@primer/octicons-react";
+
+function Navbar() {
   return (
     <>
       <Header></Header>
+      <ActionMenu>
+        <ActionMenu.Button>Open menu</ActionMenu.Button>
+        <ActionMenu.Overlay width="auto">
+          <ActionList>
+            <ActionList.Item
+              onSelect={() => alert("Workflows clicked")}
+              inactiveText="Unavailable due to an outage"
+            >
+              Workflows
+              <ActionList.LeadingVisual>
+                <WorkflowIcon />
+              </ActionList.LeadingVisual>
+            </ActionList.Item>
+            <ActionList.Item
+              onSelect={() => alert("Archived items clicked")}
+              inactiveText="Unavailable due to an outage"
+            >
+              Archived items
+              <ActionList.LeadingVisual>
+                <ArchiveIcon />
+              </ActionList.LeadingVisual>
+            </ActionList.Item>
+            <ActionList.LinkItem
+              href="/"
+              inactiveText="Unavailable due to an outage"
+            >
+              Settings
+              <ActionList.LeadingVisual>
+                <GearIcon />
+              </ActionList.LeadingVisual>
+            </ActionList.LinkItem>
+            <ActionList.Item
+              onSelect={() => alert("Make a copy clicked")}
+              inactiveText="Unavailable due to an outage"
+            >
+              Make a copy
+              <ActionList.LeadingVisual>
+                <CopyIcon />
+              </ActionList.LeadingVisual>
+            </ActionList.Item>
+            <ActionList.Divider />
+            <ActionList.Group>
+              <ActionList.GroupHeading>Github projects</ActionList.GroupHeading>
+              <ActionList.LinkItem href="/">
+                What&apos;s new
+                <ActionList.LeadingVisual>
+                  <RocketIcon />
+                </ActionList.LeadingVisual>
+              </ActionList.LinkItem>
+              <ActionList.LinkItem href="/">
+                Give feedback
+                <ActionList.LeadingVisual>
+                  <CommentIcon />
+                </ActionList.LeadingVisual>
+              </ActionList.LinkItem>
+              <ActionList.LinkItem href="/">
+                GitHub Docs
+                <ActionList.LeadingVisual>
+                  <BookIcon />
+                </ActionList.LeadingVisual>
+              </ActionList.LinkItem>
+            </ActionList.Group>
+          </ActionList>
+        </ActionMenu.Overlay>
+      </ActionMenu>
     </>
   );
-};
+}
 
-export default navbar;
+export default Navbar;
--- habbi-app/src/app/page.tsx
+++ habbi-app/src/app/page.tsx
@@ -1,12 +1,10 @@
 import { BaseStyles, Box, Heading, Button, Header } from "@primer/react";
-import { BeakerIcon, ZapIcon } from "@primer/octicons-react";
-import navbar from "./navbar";
+import Navbar from "./Navbar";
 
 export default function Page() {
   return (
     <>
-      {navbar()}
-      <BeakerIcon></BeakerIcon>
+      <Navbar></Navbar>
     </>
   );
 }
